55bd44a2e5b4b652421aee62ca3072b5b8adfd66
Franki Callard <francescalily@outlook.com>
2023-10-15 14:45:52 +0100

    ios simulator not working but expo on phone working as should

--- components/jobdetails/footer/Footer.jsx
+++ components/jobdetails/footer/Footer.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { View, Text, TouchableOpacity, Image, Linking } from "react-native";
 
 import styles from "./footer.style";
@@ -7,12 +6,19 @@ import { icons } from "../../../constants";
 const Footer = ({ url }) => {
   return (
     <View style={styles.container}>
-      <TouchableOpacity>
+      <TouchableOpacity style={styles.likeBtn}>
         <Image
-          source={icons.heart}
+          source={icons.heartOutline}
           resizeMode="contain"
           style={styles.likeBtnImage}
         />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.applyBtn}
+        onPress={() => Linking.openURL(url)}
+      >
+        <Text style={styles.applyBtnText}>Apply for job</Text>
       </TouchableOpacity>
     </View>
   );
